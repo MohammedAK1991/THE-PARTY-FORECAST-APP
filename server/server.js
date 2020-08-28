2ddd9d9ead29133d4862913dad095905466e2552
Mohammed <mohmed.ak.17@gmail.com>
2020-08-28 16:44:27 +0200

    install and use  morgan on server side

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan')
 const partyRouter = require('./router.js');
 
 const app = express();
@@ -8,6 +9,7 @@ const port = 3001;
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true })); //from cloudinary
 app.use(cors());
+app.use(morgan('tiny'));
 
 app.use('/', partyRouter);
 
