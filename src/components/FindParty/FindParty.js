7e032ecdfda4c9ef0323e89e85f15652b281818d
Mohammed <mohmed.ak.17@gmail.com>
2020-08-28 15:47:53 +0200

    refactor FIndParty componenent to import Search componenet

--- src/components/FindParty/FindParty.js
+++ src/components/FindParty/FindParty.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import axios from "axios";
 import { Image, Transformation } from 'cloudinary-react';
 import Locate from '../Locate/Locate';
+import Search from '../Search/Search';
 
 import moment from "moment";
 import '../../index.css'
@@ -369,88 +370,64 @@ export default function FindParty() {
         {partyList ? (
           <PartyList partyList={partyList} {...panTo} handlePanTo={handlePanTo} />
         ) : <h1>No parties in list </h1>}
-
       </div>
-
     </div>
   );
 }
 
-// function Locate({ panTo }) {
+// function Search({ panTo }) {
+//   const {
+//     ready,
+//     value,
+//     suggestions: { status, data },
+//     setValue,
+//     clearSuggestions,
+//   } = usePlacesAutocomplete({
+//     requestOptions: {
+//       location: { lat: () => 41.3851, lng: () => 2.1734 },
+//       radius: 100 * 1000,
+//     },
+//   });
+
+//   const handleInput = (e) => {
+//     setValue(e.target.value);
+//   };
+
+//   const handleSelect = async (address) => {
+//     setValue(address, false);
+//     clearSuggestions();
+
+//     try {
+//       const results = await getGeocode({ address });
+//       const { lat, lng } = await getLatLng(results[0]);
+//       panTo({ lat, lng });
+//     } catch (error) {
+//       console.log("😱 Error: ", error);
+//     }
+//   };
+
 //   return (
-//     <button
-//       className="locate"
-//       onClick={(e) => {
-//         e.preventDefault()
-//         window.navigator.geolocation.getCurrentPosition(
-//           (position) => {
-//             panTo({
-//               lat: position.coords.latitude,
-//               lng: position.coords.longitude,
-//             });
-//           },
-//           () => null
-//         );
-//       }}
-//     >
-//       <img src="/compass.svg" alt="compass" />
-//     </button>
+//     <div className="search">
+//       <Combobox onSelect={handleSelect} className="ui icon input">
+//         <ComboboxInput
+//           value={value}
+//           onChange={handleInput}
+//           disabled={!ready}
+//           placeholder="Search for location or click on compass"
+//           className="ui_search"
+//           id="ui_search"
+//           style={{ fontFamily: 'Avenir', color: 'white' }}
+//         />
+//         <i aria-hidden="false" className="search icon large" id="search_icon"></i>
+//         <ComboboxPopover>
+//           <ComboboxList>
+//             {status === "OK" &&
+//               data.map(({ id, description }) => (
+//                 <ComboboxOption key={id + Math.random() * 1000} value={description} style={{ fontFamily: 'Avenir', color: 'white', backgroundColor: 'black' }} />
+//               ))}
+//           </ComboboxList>
+//         </ComboboxPopover>
+//       </Combobox>
+//     </div>
 //   );
 // }
-
-function Search({ panTo }) {
-  const {
-    ready,
-    value,
-    suggestions: { status, data },
-    setValue,
-    clearSuggestions,
-  } = usePlacesAutocomplete({
-    requestOptions: {
-      location: { lat: () => 41.3851, lng: () => 2.1734 },
-      radius: 100 * 1000,
-    },
-  });
-
-  const handleInput = (e) => {
-    setValue(e.target.value);
-  };
-
-  const handleSelect = async (address) => {
-    setValue(address, false);
-    clearSuggestions();
-
-    try {
-      const results = await getGeocode({ address });
-      const { lat, lng } = await getLatLng(results[0]);
-      panTo({ lat, lng });
-    } catch (error) {
-      console.log("😱 Error: ", error);
-    }
-  };
-
-  return (
-    <div className="search">
-      <Combobox onSelect={handleSelect} className="ui icon input">
-        <ComboboxInput
-          value={value}
-          onChange={handleInput}
-          disabled={!ready}
-          placeholder="Search for location or click on compass"
-          className="ui_search"
-          id="ui_search"
-          style={{ fontFamily: 'Avenir', color: 'white' }}
-        />
-        <i aria-hidden="false" className="search icon large" id="search_icon"></i>
-        <ComboboxPopover>
-          <ComboboxList>
-            {status === "OK" &&
-              data.map(({ id, description }) => (
-                <ComboboxOption key={id + Math.random() * 1000} value={description} style={{ fontFamily: 'Avenir', color: 'white', backgroundColor: 'black' }} />
-              ))}
-          </ComboboxList>
-        </ComboboxPopover>
-      </Combobox>
-    </div>
-  );
-}
